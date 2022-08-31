e04e1c24d0b04d6f6714c73c525eca7a5a878735
Jean C. Contreras <jean.cntrs@gmail.com>
2022-08-31 15:03:11 -0400

    Added GetStaticProps in Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
-import { NextPage } from 'next';
+import { NextPage, GetStaticProps } from 'next';
 import { Button } from '@nextui-org/react';
 import { Layout } from '../components/layouts';
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+  console.log('props1', props);
+
   return (
     <Layout title='Pokemon List'>
       <Button color='gradient'> Hello Again!</Button>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  console.log('hello worldddd');
+
+  return {
+    props: {
+      name: 'jc'
+    }
+  }
 }
 
 export default Home
