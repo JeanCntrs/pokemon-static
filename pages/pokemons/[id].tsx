290801bd83f9639cf698bdb89580e50e107f0a89
Jean C. Contreras <jean.cntrs@gmail.com>
2022-09-03 11:53:12 -0400

    Added 151 pages for pokemons

--- pages/pokemons/[id].tsx
+++ pages/pokemons/[id].tsx
@@ -1,11 +1,39 @@
+import { NextPage, GetStaticProps, GetStaticPaths } from "next";
+
+import { pokeApi } from '../../api';
+import { Layout } from "../../components/layouts";
+import { PokemonFull } from '../../interfaces';
+
 interface PokemonPageProps {
-    
+    pokemon: PokemonFull
 }
- 
-const PokemonPage: React.FC<PokemonPageProps> = () => {
-    return (  
-        <div></div>
+
+const PokemonPage: NextPage<PokemonPageProps> = ({ pokemon }) => {
+    return (
+        <Layout title="Some pokémon">
+            <h1>{pokemon.name}</h1>
+        </Layout>
     );
 }
- 
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+    const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`)
+
+    return {
+        paths: pokemons151.map(id => ({ params: { id } })),
+        fallback: false
+    }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    const { id } = ctx.params as { id: string };
+    const { data } = await pokeApi.get<PokemonFull>(`/pokemon/${id}`);
+
+    return {
+        props: {
+            pokemon: data
+        }
+    }
+}
+
 export default PokemonPage;
